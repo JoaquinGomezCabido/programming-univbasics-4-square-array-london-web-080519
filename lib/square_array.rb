b00ef4f8282d08af31106eb9f9108fbd3680422a
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-13 08:41:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array = []
   counter = 0
   while counter < array.length do
-    new_array[counter] = array[counter] ** array[counter]
+    new_array[counter] = array[counter] ** 2
     counter += 1
   end
   return new_array
